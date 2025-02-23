d2d33a61cd2679b8c01dfa0a0333509e2c871af7
Sohrab300 <133908818+Sohrab300@users.noreply.github.com>
2025-02-23 18:00:39 +0530

    testing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Logo from "/logo-2.svg";
 
 const Navbar = ({ onlineCount }) => {
   return (
     <div className="container flex justify-between items-center px-12 min-w-full bg-pink-200 h-[10vh]">
       <div className="logo w-40 h-12 flex justify-center items-center text-3xl font-bold">
-        <img src="src/assets/logo-2.svg" alt="Logo" />
+        <img src={Logo} alt="Logo" />
       </div>
       <div className="onluser flex items-center h-[50%] w-fit rounded-md bg-[#f7f2f3] p-2">
         <span>Users online: {onlineCount}</span>
